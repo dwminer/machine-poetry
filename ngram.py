d5cd2381d2d20340681a2fcfd079d8c1c60fc4ae
dwminer <daltonminer@gmail.com>
2016-05-03 20:04:46 -0400

    Fix ngram always picking randomly

--- ngram.py
+++ ngram.py
@@ -7,6 +7,7 @@ import random, itertools, bisect
 
 class Model:
 	def __init__(self, sents, n=2, cpd_class=nltk.MLEProbDist):
+		self.debug = True
 		self.N   = n
 		self.CPD_CLASS = cpd_class
 		self.cfd = nltk.ConditionalFreqDist()
@@ -32,19 +33,33 @@ class Model:
 	
 	# Deterministically chooses the most probable word from wordlist based on the model
 	def choose_max(self, prev, wordlist):
-		prev = tuple(prev)
-		return max((self.cpd[prev].prob(w.lower()), w) for w in wordlist)[1]
+		prev = tuple([w.lower() for w in prev])
+		prob, choice = max((self.cpd[prev].prob(w.lower()), w) for w in wordlist)
+		if self.debug:
+			print("Returning {} (probability {}%)".format(choice, 100 * prob))
+		return choice
+
 
 	# Randomly chooses a word from wordlist weighted by their likelihood in the model
 	def choose_weighted_random(self, prev, wordlist):
-		prev = tuple(prev)
+		prev = tuple([w.lower() for w in prev])
 		choices, weights = zip(*((w, self.cpd[prev].prob(w.lower())) for w in wordlist))
 		cumulative_dist = list(itertools.accumulate(weights))
 		x = random.random() * cumulative_dist[-1]
 		if max(weights) > 0:
-			return choices[bisect.bisect(cumulative_dist, x)]
+			choice = choices[bisect.bisect(cumulative_dist, x)]
+			if self.debug:
+				print("Returning {} (probability {}%) from {}, prev={}".format(
+				                                               choice,
+				                                               100 * self.cpd[prev].prob(choice.lower()),
+				                                               len(choices),
+				                                               prev))
+			return choice
 		else:
-			return random.choice(wordlist)
+			choice = random.choice(wordlist)
+			if self.debug:
+				print("Randomly returning a word from {}, prev={}".format(len(wordlist), prev))
+			return choice
 	
 	# Chooses a word from wordlist completely randomly, excepting words not in the dictionary (unless we've no other choice)
 	def choose_random(self, prev, wordlist):
